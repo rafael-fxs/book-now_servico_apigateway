83fce6dcb03eda361a93783f4a61b2d4cacf3187
silva.xavier <silva.xavier@pucpr.edu.br>
2024-08-27 21:16:47 -0300

    Configurado Gateway

---
+++ src/main/java/com/booknow/apigateway/ApigatewayApplication.java
@@ -0,0 +1,28 @@
+package com.booknow.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Configuration
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/transacoes/**")
+						.uri("http://localhost:8083/api/transacoes"))
+				.build();
+	}
+}
