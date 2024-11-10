8b389ac2cbb4f52698f0b1a57f2fcc28d86769c1
Rafael Felipe <rafa_el_felipe@hotmail.com>
2024-11-10 16:34:25 -0300

    Tratado mensagens de erro

--- src/main/java/com/booknow/apigateway/JwtValidationGatewayFilter.java
+++ src/main/java/com/booknow/apigateway/JwtValidationGatewayFilter.java
@@ -3,11 +3,15 @@ import jakarta.ws.rs.core.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 public class JwtValidationGatewayFilter implements GatewayFilter {
     private final WebClient webClient;
@@ -31,8 +35,21 @@ public class JwtValidationGatewayFilter implements GatewayFilter {
                 .uri("/auth/validate")
                 .header(HttpHeaders.AUTHORIZATION, authHeader)
                 .retrieve()
-                .onStatus(HttpStatusCode::isError, clientResponse -> Mono.error(new RuntimeException("Token inválido")))
+                .onStatus(HttpStatusCode::isError, clientResponse -> {
+                    // Log or handle the error here if needed
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return Mono.error(new RuntimeException("Token inválido"));
+                })
                 .toBodilessEntity()
-                .then(chain.filter(exchange));
+                .then(chain.filter(exchange))
+                .onErrorResume(e -> this.buildErrorResponse(exchange, e.getMessage()));
+    }
+
+    private Mono<Void> buildErrorResponse(ServerWebExchange exchange, String errorMessage) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        byte[] bytes = errorMessage.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+        return exchange.getResponse().writeWith(Mono.just(buffer));
     }
 }
